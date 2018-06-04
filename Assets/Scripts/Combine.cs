70f0cb6fd7bf833d914cf54dcc740dd88e28c9da
John Fouts <shannonke@gatech.edu>
2018-06-04 16:46:16 -0400

    started putting colliders manually around the cart and bins. need to figure out how to move the cart without everything exploding

---
+++ Assets/Scripts/Combine.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+public class Combine : MonoBehaviour
+{
+    void Start()
+    {
+        MeshFilter[] meshFilters = GetComponentsInChildren<MeshFilter>();
+        CombineInstance[] combine = new CombineInstance[meshFilters.Length];
+        int i = 0;
+        while (i < meshFilters.Length)
+        {
+            combine[i].mesh = meshFilters[i].sharedMesh;
+            combine[i].transform = meshFilters[i].transform.localToWorldMatrix;
+            meshFilters[i].gameObject.SetActive(false);
+            i++;
+        }
+        //transform.GetComponent<MeshFilter>().mesh = new Mesh();
+        transform.GetComponent<MeshFilter>().mesh.CombineMeshes(combine);
+        Mesh newMesh = transform.GetComponent<MeshFilter>().mesh;
+        MeshCollider collider = gameObject.AddComponent<MeshCollider>();
+        collider.sharedMesh = newMesh;
+        collider.convex = true;
+        transform.gameObject.SetActive(true);
+    }
+}
