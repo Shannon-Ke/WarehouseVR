9e002430c8a4d93a73db1e3032fb482336d8edf6
John Fouts <shannonke@gatech.edu>
2018-06-13 14:42:24 -0400

    started scenario 3. Put in holographic bar graphs and scatter plot. can toggle between them and hide and show bars on the graphs

---
+++ Assets/Scripts/ControllerScenThree.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//attached to the left hand! allows scanning of barcodes and toggling of the log on and off
+public class ControllerScenThree : MonoBehaviour
+{
+    public GameObject map;
+    public GameObject button;
+    private SteamVR_TrackedObject trackedObj;
+    bool on;
+    
+    private SteamVR_Controller.Device Controller
+    {
+        get { return SteamVR_Controller.Input((int)trackedObj.index); }
+    }
+    
+    void Awake()
+    {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+        on = false;
+     
+       
+    }
+
+  
+    // Update is called once per frame
+    void Update()
+    {
+      
+        if (Controller.GetPressDown(SteamVR_Controller.ButtonMask.Touchpad) && !on)
+        {
+            map.SetActive(true);
+            button.SetActive(true);
+            on = true;
+        } else if (Controller.GetPressDown(SteamVR_Controller.ButtonMask.Touchpad) && on)
+        {
+            //close.Play("close");
+            map.SetActive(false);
+            button.SetActive(false);
+            on = false;
+        }
+        
+       
+        if (Controller.GetPressDown(SteamVR_Controller.ButtonMask.Grip))
+        {
+            Debug.Log("Could be used later");
+        }
+    }
+
+}
---
+++ Assets/Scripts/Hide.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hide : MonoBehaviour {
+    public GameObject color;
+    // Use this for initialization
+    public static Hide hide;
+}
