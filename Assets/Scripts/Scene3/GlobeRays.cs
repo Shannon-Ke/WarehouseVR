d8917be7fb65a6ce4d79b12f31cba949e31cc1cc
John Fouts <shannonke@gatech.edu>
2018-07-03 16:19:06 -0400

    added curved lines, separated globe into separate scene, added tooltips

---
+++ Assets/Scripts/Scene3/GlobeRays.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobeRays : MonoBehaviour {
+
+    private void Start()
+    {
+        DrawLines();
+    }
+    void DrawLines()
+    {
+        foreach (Transform t in GetComponentsInChildren<Transform>())
+        {
+            Vector3 distance = t.position - transform.position;
+            GameObject newVector = new GameObject("newVector");
+            newVector.transform.position = transform.position;
+            newVector.transform.localScale = distance;
+            
+
+        }
+    }
+}
--- Assets/Scripts/Scene5/WalkUI.cs
+++ Assets/Scripts/Scene5/WalkUI.cs
@@ -7,14 +7,17 @@ public class WalkUI : MonoBehaviour {
     public Animation aisle1;
     public Animation aisle2;
     public GameObject question;
+    bool played = false;
    
     public void OnTriggerEnter(Collider other)
     {
-        if (other.isTrigger && other.name == "Eye")
+        if (!played && other.isTrigger && other.name == "Eye")
         {
             question.SetActive(true);
             aisle1.Play();
+          
             aisle2.Play();
+            played = true;
         }
     }
 }
